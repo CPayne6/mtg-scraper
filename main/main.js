562672dec08812ef4cb90d7913058344fdeca2dc
Chris Payne <cpaynetrayne6@gmail.com>
2025-09-07 10:15:00 -0400

    electron configuration fixed

--- main/main.js
+++ main/main.js
@@ -1,10 +1,15 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, shell } from "electron";
 import serve from "electron-serve";
-import { join } from "path";
+import path, { join } from "path";
+import { fileURLToPath } from "url"
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const appServe = app.isPackaged ? serve({
   directory: join(__dirname, "../out")
 }) : null;
+
+console.log(join(__dirname, "preload.js"))
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -13,6 +18,16 @@ const createWindow = () => {
     webPreferences: {
       preload: join(__dirname, "preload.js")
     }
+  });
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    // Add logic here to determine if the URL should be opened externally
+    // For example, check if the host is different from your app's host
+    if (url.startsWith('http:') || url.startsWith('https:')) {
+      shell.openExternal(url);
+      return { action: 'deny' }
+    }
+    return { action: 'allow' }
   });
 
   if (app.isPackaged) {
@@ -29,11 +44,11 @@ const createWindow = () => {
 }
 
 app.on("ready", () => {
-    createWindow();
+  createWindow();
 });
 
 app.on("window-all-closed", () => {
-    if(process.platform !== "darwin"){
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
--- src/app/list/[source]/[id]/page.tsx
+++ src/app/list/[source]/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { CardDisplay } from '@/components/CardDisplay/CardDisplay';
 import { MoxfieldLoader, DeckLoader } from '@/scraper/loaders'
-import { Heading, Text, Center, Flex, Image, Stack, Box } from '@chakra-ui/react'
+import { Heading, Text, Center, Flex, Image, Stack, Box, Button } from '@chakra-ui/react'
 
 const sourceMap: Record<string, DeckLoader> = {
   moxfield: new MoxfieldLoader()
+}
+
+export function generateStaticParams() {
+  return []
 }
 
 export default async function Page({
@@ -26,8 +30,10 @@ export default async function Page({
       <Box gap="5">
         <Stack direction="row" gap="5" align="center">
           <Image src="/scanner.png" alt="logo" height="100x" width="200px" />
-          <Stack><Heading size="4xl">Browse Cards</Heading>
+          <Stack>
+            <Heading size="4xl">Browse Cards</Heading>
             <Text>Find the cards that you want at the best prices</Text>
+            <Button asChild><a href="/">Home</a></Button>
           </Stack>
         </Stack>
         <CardDisplay
