e3479dac60f0b0a4538f9abffc97850bf6436099
Chris Payne <cpaynetrayne6@gmail.com>
2025-09-16 02:04:37 -0400

    feat: added icon and page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
+export const metadata = {
+  title: 'MTG Scraper',
+  description: 'A web scraper for MTG cards',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
