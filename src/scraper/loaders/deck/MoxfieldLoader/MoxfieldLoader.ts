c52c4d4f3ab6c873cb4ee0c169da88579322c0bc
Chris Payne <cpaynetrayne6@gmail.com>
2025-09-24 01:49:29 -0400

    feat: commanders and companions added to moxfield fetch

--- src/scraper/loaders/deck/MoxfieldLoader/MoxfieldLoader.ts
+++ src/scraper/loaders/deck/MoxfieldLoader/MoxfieldLoader.ts
@@ -9,6 +9,16 @@ export class MoxfieldLoader extends DeckLoader {
   protected parseCardNames(data: string): string[] {
     const cardNames: string[] = []
     const json: MoxfieldResponse = JSON.parse(data)
+    for (const card of Object.values(json?.boards?.commanders?.cards ?? {})) {
+      cardNames.push(card.card.name)
+    }
+    for (const card of Object.values(json?.boards?.companions?.cards ?? {})) {
+      // @ts-ignore
+      if (card?.card?.name) {
+        // @ts-ignore
+        cardNames.push(card?.card?.name)
+      }
+    }
     for (const card of Object.values(json?.boards?.mainboard?.cards ?? {})) {
       if (card?.card?.name) {
         cardNames.push(card.card.name)
