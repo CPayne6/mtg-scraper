d0fd8170823f28ed88b340dd5e4305ae12f9d657
Chris Payne <cpaynetrayne6@gmail.com>
2026-05-17 02:13:28 -0400

    batch DB writes in storefront extraction

--- apps/scraper/src/extraction/extraction.module.ts
+++ apps/scraper/src/extraction/extraction.module.ts
@@ -41,6 +41,6 @@ import { TokenListingUpsertService } from './token-listing-upsert.service';
     TokenBatchAccumulatorService,
     TokenListingUpsertService,
   ],
-  exports: [ExtractionService],
+  exports: [ExtractionService, PrintingMatcherService],
 })
 export class ExtractionModule {}
