17791e5f3d3cb1ae4197319b728ca3592b9adb19
Chris Payne <cpaynetrayne6@gmail.com>
2026-05-17 02:13:22 -0400

    add rate limiting, backpressure, and run tracking to discovery

--- apps/scraper/src/discovery/discovery.module.ts
+++ apps/scraper/src/discovery/discovery.module.ts
@@ -5,20 +5,24 @@ import {
   CacheModule,
   PlatformModule,
   ProxyModule,
+  RateLimiterModule,
   Store,
   ProductUrl,
   MtgSinglesCollection,
+  InvalidProductHandle,
+  DiscoveryRun,
 } from '@scoutlgs/core';
 import { DiscoveryService } from './discovery.service';
 import { DiscoveryProcessor } from './discovery.processor';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Store, ProductUrl, MtgSinglesCollection]),
+    TypeOrmModule.forFeature([Store, ProductUrl, MtgSinglesCollection, InvalidProductHandle, DiscoveryRun]),
     QueueModule,
     CacheModule,
     PlatformModule,
     ProxyModule,
+    RateLimiterModule,
   ],
   providers: [DiscoveryService, DiscoveryProcessor],
   exports: [DiscoveryService],
